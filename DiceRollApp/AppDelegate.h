2ded4acc0ae4d8b613608c5a2e7aed7a18b08d4b
Ratheesh Reddy <ratheesh@MacBook-Pro.local>
2014-09-02 16:51:58 -0400

    Initial Commit

---
+++ DiceRollApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DiceRollApp
+//
+//  Created by Ratheesh Reddy on 9/2/14.
+//  Copyright (c) 2014 Ventois. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DiceRollApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DiceRollApp
+//
+//  Created by Ratheesh Reddy on 9/2/14.
+//  Copyright (c) 2014 Ventois. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
